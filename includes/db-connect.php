9816c5ff99dcb7c38dda4ff615565e73060546d6
Michael Wood <113477758+mikewood17@users.noreply.github.com>
2023-03-08 15:29:04 +0000

    database connection changes

--- includes/db-connect.php
+++ includes/db-connect.php
@@ -1,7 +1,7 @@
 <?php
 
 function OpenCon(){
-    $dbHost = "127.0.0.1";
+    $dbHost = "localhost";
     $dbUsername = "michaelw_michaelwood";
     $dbPassword = "MeggyCat!123";
     $dbName = "michaelw_NM-enquiry";
@@ -9,7 +9,19 @@ function OpenCon(){
 $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName) or die("Connection Failed");
 
 return $db;
+echo "connection successful";
 }
 
+// try {
+//     $servername = "localhost:3306";
+//     $username = "michaelw_michaelwood";
+//     $password = "MeggyCat!123";
+
+//     $connection = new PDO("mysql:host=$servername; dbname=michaelw_NM-enquiry", $username, $password);
+//     $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch(Exception $e) {
+//     echo "error!: " . $e -> getMessage();
+//     die();
+// }
 
 ?>
