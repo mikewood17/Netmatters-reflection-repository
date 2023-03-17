ecc5d13a23b078c1224dd030380268ddfea935d6
Michael Wood <113477758+mikewood17@users.noreply.github.com>
2023-03-17 11:57:12 +0000

    styling changes as per kayleighs feedback

--- includes/news.php
+++ includes/news.php
@@ -1,9 +1,20 @@
 <?php
 
-$servername = "localhost:3306";
-$username = "michaelw_michaelwood";
-$password = "MeggyCat!123";
-$dbname = "michaelw_news_section";
+// connect to hosted db
+
+// $servername = "localhost:3306";
+// $username = "michaelw_michaelwood";
+// $password = "MeggyCat!123";
+// $dbname = "michaelw_news_section";
+
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// connect to local db
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "nm-news";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
